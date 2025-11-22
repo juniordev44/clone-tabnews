e5e6bb6794ee7b2b5296c263720cb5b943b79f41
Junior <juninhogamer4k@gmail.com>
2025-11-22 23:51:47 +0000

    adiciona scripts  `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Lolo e Cris, eu amo vocês, Se vocês me amam, deem uma risadinha! 😎</h1>
+  return (
+    <h1>Lolo e Cris, eu amo vocês, Se vocês me amam, deem uma risadinha! 😎</h1>
+  );
 }
 
 export default Home;
